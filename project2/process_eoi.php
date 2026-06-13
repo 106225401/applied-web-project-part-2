0c94d9b90eaeb5062316bc448a779cf1246feca7
Jingyee <106225401@student.swin.edu.au>
2026-06-13 22:58:42 +0800

    Create EOI database table if not exists

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -13,4 +13,24 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
+
+    $create_table = "
+    CREATE TABLE IF NOT EXISTS eoi (
+        EOInumber INT AUTO_INCREMENT PRIMARY KEY,
+        jobref VARCHAR(5) NOT NULL,
+        fname VARCHAR(20) NOT NULL,
+        lname VARCHAR(20) NOT NULL,
+        dob VARCHAR(10) NOT NULL,
+        gender VARCHAR(20) NOT NULL,
+        street VARCHAR(40) NOT NULL,
+        suburb VARCHAR(40) NOT NULL,
+        state VARCHAR(3) NOT NULL,
+        postcode VARCHAR(4) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        phone VARCHAR(12) NOT NULL,
+        skill TEXT,
+        others TEXT,
+        status ENUM('New','Current','Final') DEFAULT 'New'
+    )";
+    mysqli_query($conn, $create_table);
 ?>
